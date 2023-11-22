37e47d4c0d4f9b6a6c422f07c44c97b4476e9378
Saga-sanga <vulcan248@gmail.com>
2023-11-22 12:03:52 +0530

    feat: add bg gradient"

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,15 @@ function App() {
   return (
     <div className="grid grid-cols-9 min-h-screen">
       <div className="bg-primary col-span-5">
-        <h2>Hello world</h2>
+        <div
+          className="w-full h-full bg-right-bottom"
+          style={{
+            background:
+              "radial-gradient(123% 120% at 18% 109%, rgba(31, 169, 17, 0.81) 26%, #2F15D0 64.58%, rgba(0, 0, 0, 0.00) 100%)",
+          }}
+        >
+          
+        </div>
       </div>
       <div className="col-span-4 flex items-center justify-center">
         <div className="w-[365px] flex flex-col items-center space-y-6">
