1d3f3a1a8e0ede4d0d70bf5fd7c200727d2364e9
Saga-sanga <vulcan248@gmail.com>
2023-11-22 18:42:33 +0530

    feat: add testemonies with drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,72 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Input } from "./components/ui/input";
+import { BellIcon, Eye, LochLogo } from "./components/icons";
+import { TestimonyList } from "./components/testimony-list";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
   return (
     <div className="grid grid-cols-9 min-h-screen">
       <div className="bg-primary col-span-5">
         <div
-          className="w-full h-full bg-right-bottom"
+          className="w-full h-full bg-right-bottom grid place-content-center py-14"
           style={{
             background:
               "radial-gradient(123% 120% at 18% 109%, rgba(31, 169, 17, 0.81) 26%, #2F15D0 64.58%, rgba(0, 0, 0, 0.00) 100%)",
           }}
         >
-          
+          <div className="flex mb-12">
+            <div className="basis-1/2 w-20 space-y-4">
+              <BellIcon />
+              <h4
+                className="text-[#F2F2F2] w-[19.5625rem]"
+                style={{
+                  textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                }}
+              >
+                Get notified when a highly correlated whale makes a move
+              </h4>
+              <p className="text-[#F2F2F2] opacity-70 w-[20.125rem]">
+                Find out when a certain whale moves more than any preset amount
+                on-chain or when a dormant whale you care about becomes active.
+              </p>
+            </div>
+            <div className="basis-1/2"></div>
+          </div>
+          <div className="flex my-5">
+            <div className="basis-1/2">
+              <img
+                alt="Cohorts"
+                src="/Cohorts.png"
+                className="shrink-0 rounded-xl ml-auto"
+                width={335}
+                height={306}
+              />
+            </div>
+            <div className="basis-1/2 text-right space-y-6 flex flex-col items-end">
+              <Eye className="ml-auto" />
+              <h4 className="text-[#F2F2F2] w-[19.0625rem]">
+                Watch what the whales are doing
+              </h4>
+              <p className="text-[#F2F2F2] opacity-70 w-[19.0625rem]">
+                All whales are not equal. Know exactly what the whales impacting
+                YOUR portfolio are doing.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-end gap-5">
+            <h5 className="text-[#F2F2F2]">Testimonials</h5>
+            <span className="border-t border-[#E5E5E6] w-full opacity-50"></span>
+          </div>
+          <div className="mt-10 flex gap-10">
+            <LochLogo className="self-end" />
+            <TestimonyList />
+          </div>
         </div>
       </div>
-      <div className="col-span-4 flex items-center justify-center">
+      <div className="col-span-4 flex bg-background z-50 items-center justify-center">
         <div className="w-[365px] flex flex-col items-center space-y-6">
-          <h1 className="font-medium text-4xl text-muted-foreground px-3">
-            Sign up for exclusive access.
-          </h1>
+          <h3 className="px-3 text-[#B0B1B3]">Sign up for exclusive access.</h3>
           <Input
             className="w-[340px] h-14 shadow px-6"
             placeholder="Your email address"
---
+++ src/components/testimony-list.tsx
@@ -0,0 +1,69 @@
+import { Card, CardContent, CardHeader } from "./ui/card";
+// import "keen-slider/keen-slider.min.css";
+// import { useKeenSlider } from "keen-slider/react";
+// import useEmblaCarousel from "embla-carousel-react";
+import { useRef } from "react";
+import { useDraggable } from "react-use-draggable-scroll";
+
+const testimonies = [
+  {
+    name: "Jack F",
+    title: "Ex Blackrock PM",
+    content:
+      "“Love how Loch integrates portfolio analytics and whale watching into one unified app.”",
+  },
+  {
+    name: "Yash P",
+    title: "Research, 3poch Crypto Hedge Fund",
+    content:
+      "“I use Loch everyday now. I don't think I could analyze crypto whale trends markets without it. I'm addicted!”",
+  },
+  {
+    name: "Shiv S",
+    title: "Co-Founder Magik Labs",
+    content:
+      "“Managing my own portfolio is helpful and well designed. What’s really interesting is watching the whales though. No one else has made whale tracking so simple.”",
+  },
+];
+
+export function TestimonyList() {
+  const ref =
+    useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+  const { events } = useDraggable(ref);
+
+  return (
+    <div className="h-[139px] relative">
+      <div className="absolute w-[60vw] h-[139px] touch-pan-x overflow-y-scroll" {...events} ref={ref}>
+        <div className="flex gap-4 absolute pr-[25vw]">
+          <Card className="flex flex-col w-[353px]">
+            <CardHeader className="flex-row space-y-0 gap-2 items-center p-5">
+              <p>{testimonies[0].name}</p>
+              <small>{testimonies[0].title}</small>
+            </CardHeader>
+            <CardContent className="p-5 pt-0">
+              <p className="">{testimonies[0].content}</p>
+            </CardContent>
+          </Card>
+          <Card className="flex flex-col w-[353px]">
+            <CardHeader className="flex-row space-y-0 gap-2 items-center p-5">
+              <p>{testimonies[1].name}</p>
+              <small>{testimonies[1].title}</small>
+            </CardHeader>
+            <CardContent className="p-5 pt-0">
+              <p className="">{testimonies[1].content}</p>
+            </CardContent>
+          </Card>
+          <Card className="flex flex-col w-[531px]">
+            <CardHeader className="flex-row space-y-0 gap-2 items-center p-5">
+              <p>{testimonies[2].name}</p>
+              <small>{testimonies[2].title}</small>
+            </CardHeader>
+            <CardContent className="p-5 pt-0">
+              <p className="">{testimonies[2].content}</p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
